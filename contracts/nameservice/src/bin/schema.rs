c9402e480962382797ffd6ae01cd3e0ffc967e5b
TTNguyenDev <nttriet9a4@gmail.com>
2024-02-11 15:08:08 +0700

    rename contract

--- contracts/nameservice/src/bin/schema.rs
+++ contracts/nameservice/src/bin/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use name_service::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use nameservice::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
